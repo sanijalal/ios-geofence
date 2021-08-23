718dd243df058a036e9849eacf70fb7fa3e7709a
Sani Jalal <binabdjalal@gmail.com>
2021-08-23 20:24:58 +0800

    Initial Commit

---
+++ GeofenceMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeofenceMe
+//
+//  Created by Sani on 23/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
