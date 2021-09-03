169db268857d056298d3c4c2116356ca41aac0e9
Sani <binabdjalal@gmail.com>
2021-09-03 12:37:24 +0800

    Removed comments in SceneDelegate.

--- GeofenceMe/SceneDelegate.swift
+++ GeofenceMe/SceneDelegate.swift
@@ -44,27 +44,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-        print("Scene did become active")
         appCoordinator.appDidBecomeActive()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
-        print("Scene will resign active")
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
-        print("Scene will enter foreground")
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-        print("Scene did enter background")
     }
 
 
